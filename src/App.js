0229da519b7be06e68532fe70da8433b6d495569
Ronald Riveros <ronaldriveros@gmail.com>
2022-06-26 01:56:28 -0400

    se carga los datos principales del clima de la ciudad elegida

--- src/App.js
+++ src/App.js
@@ -1,251 +1,307 @@
 import "./App.css";
 import mostlySunny from "./icons/mostly-sunny.svg";
 import sunny from "./icons/sunny.svg";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+	const [weather, setWeather] = useState([]);
+
+	useEffect(() => {
+		getWheater(
+			document.getElementById("slcCity").options[
+				document.getElementById("slcCity").selectedIndex
+			].value
+		);
+	}, []);
+
+	async function getWheater(query) {
+		await axios
+			.get(
+				`http://api.openweathermap.org/data/2.5/weather?q=${query}&lang=es&units=metric&APPID=7d3e3f41015bfd372c5d4d66b8aa3e51`
+			)
+			.then((response) => {
+				console.log(response.data);
+				setWeather(response.data);
+			});
+	}
+
+	function getDate() {
+		const date = new Date();
+		return `${date.toLocaleDateString("es-ES", {
+			weekday: "long",
+			month: "long",
+			day: "numeric",
+		})}`;
+	}
+
+	function convertTimestampToHour(timestamp) {
+		let date = new Date(timestamp * 1000);
+		let hours = "0" + date.getHours();
+		let minutes = "0" + date.getMinutes();
+
+		return hours.substr(-2) + ":" + minutes.substr(-2);
+	}
+
 	return (
 		<div className="App container">
-			<main class="main-container">
+			<main className="main-container">
 				<select
-					class="form-select form-select-lg mb-3"
+					id="slcCity"
+					className="form-select form-select-lg mb-3"
 					aria-label=".form-select-lg example"
+					onChange={(e) => getWheater(e.target.value)}
 				>
-					<option selected value="0">
-						Asunción, Paraguay
-					</option>
-					<option value="1">Buenos Aires, Argentina</option>
-					<option value="2">New York, Estados Unidos</option>
-					<option value="3">Madrid, España</option>
-					<option value="4">Pekín, China</option>
+					<option value="asuncion,py">Asunción, Paraguay</option>
+					<option value="buenos%20aires,ar">Buenos Aires, Argentina</option>
+					<option value="new%20york,us">New York, Estados Unidos</option>
+					<option value="madrid,es">Madrid, España</option>
+					<option value="beijing,cn">Beijing, China</option>
 				</select>
 
-				<div class="location-and-date">
-					<div>Domingo, 19 de Junio</div>
+				<div className="location-and-date">
+					<div>{getDate()}</div>
 				</div>
 
-				<div class="current-temperature">
-					<div class="current-temperature__icon-container">
-						<img src={mostlySunny} class="current-temperature__icon" alt="" />
+				<div className="current-temperature">
+					<div className="current-temperature__icon-container">
+						<img
+							src={mostlySunny}
+							className="current-temperature__icon"
+							alt=""
+						/>
 					</div>
-					<div class="current-temperature__content-container">
-						<div class="current-temperature__value">21&deg;</div>
-						<div class="current-temperature__summary">Mayormente Soleado</div>
-					</div>
-				</div>
-
-				<div class="current-stats">
-					<div>
-						<div class="current-stats__value">23&deg;</div>
-						<div class="current-stats__label">Max</div>
-						<div class="current-stats__value">14&deg;</div>
-						<div class="current-stats__label">Min</div>
-					</div>
-					<div>
-						<div class="current-stats__value">7mph</div>
-						<div class="current-stats__label">Viento</div>
-						<div class="current-stats__value">0%</div>
-						<div class="current-stats__label">Lluvia</div>
-					</div>
-					<div>
-						<div class="current-stats__value">05:27</div>
-						<div class="current-stats__label">Amanecer</div>
-						<div class="current-stats__value">20:57</div>
-						<div class="current-stats__label">Ocaso</div>
+					<div className="current-temperature__content-container">
+						<div className="current-temperature__value">{weather.main?.temp.toFixed(1)}&deg;</div>
+						<div className="current-temperature__summary">
+							{weather.weather[0]?.description}
+						</div>
 					</div>
 				</div>
 
-				<div class="weather-by-hour">
-					<h2 class="weather-by-hour__heading">Clima de Hoy</h2>
-					<div class="weather-by-hour__container">
-						<div class="weather-by-hour__item">
-							<div class="weather-by-hour__hour">3am</div>
+				<div className="current-stats">
+					<div>
+						<div className="current-stats__value">
+							{weather.main?.temp_max.toFixed(1)}&deg;
+						</div>
+						<div className="current-stats__label">Max</div>
+						<div className="current-stats__value">
+							{weather.main?.temp_min.toFixed(1)}&deg;
+						</div>
+						<div className="current-stats__label">Min</div>
+					</div>
+					<div>
+						<div className="current-stats__value">
+							{weather.wind?.speed.toFixed(0)}kmh
+						</div>
+						<div className="current-stats__label">Viento</div>
+						<div className="current-stats__value">{weather.main?.humidity.toFixed(0)}%</div>
+						<div className="current-stats__label">Humedad</div>
+					</div>
+					<div>
+						<div className="current-stats__value">
+							{convertTimestampToHour(weather.sys?.sunrise)}
+						</div>
+						<div className="current-stats__label">Amanecer</div>
+						<div className="current-stats__value">
+							{convertTimestampToHour(weather.sys?.sunset)}
+						</div>
+						<div className="current-stats__label">Ocaso</div>
+					</div>
+				</div>
+
+				<div className="weather-by-hour">
+					<h2 className="weather-by-hour__heading">Clima de Hoy</h2>
+					<div className="weather-by-hour__container">
+						<div className="weather-by-hour__item">
+							<div className="weather-by-hour__hour">3am</div>
 							<img src={mostlySunny} alt="Mayormente Soleado" />
 							<div>14&deg;</div>
 						</div>
-						<div class="weather-by-hour__item">
-							<div class="weather-by-hour__hour">6am</div>
+						<div className="weather-by-hour__item">
+							<div className="weather-by-hour__hour">6am</div>
 							<img src={mostlySunny} alt="Mayormente Soleado" />
 							<div>16&deg;</div>
 						</div>
-						<div class="weather-by-hour__item">
-							<div class="weather-by-hour__hour">9am</div>
+						<div className="weather-by-hour__item">
+							<div className="weather-by-hour__hour">9am</div>
 							<img src={mostlySunny} alt="Mayormente Soleado" />
 							<div>17&deg;</div>
 						</div>
-						<div class="weather-by-hour__item">
-							<div class="weather-by-hour__hour">12pm</div>
+						<div className="weather-by-hour__item">
+							<div className="weather-by-hour__hour">12pm</div>
 							<img src={mostlySunny} alt="Mayormente Soleado" />
 							<div>19&deg;</div>
 						</div>
-						<div class="weather-by-hour__item">
-							<div class="weather-by-hour__hour">3pm</div>
+						<div className="weather-by-hour__item">
+							<div className="weather-by-hour__hour">3pm</div>
 							<img src={sunny} alt="Sunny" />
 							<div>21&deg;</div>
 						</div>
-						<div class="weather-by-hour__item">
-							<div class="weather-by-hour__hour">6pm</div>
+						<div className="weather-by-hour__item">
+							<div className="weather-by-hour__hour">6pm</div>
 							<img src={sunny} alt="Sunny" />
 							<div>20&deg;</div>
 						</div>
-						<div class="weather-by-hour__item">
-							<div class="weather-by-hour__hour">9pm</div>
+						<div className="weather-by-hour__item">
+							<div className="weather-by-hour__hour">9pm</div>
 							<img src={mostlySunny} alt="Mayormente Soleado" />
 							<div>18&deg;</div>
 						</div>
 					</div>
 				</div>
 
-				<div class="next-5-days">
-					<h2 class="next-5-days__heading">Siguientes 5 días</h2>
-					<div class="next-5-days__container">
-						<div class="next-5-days__row">
-							<div class="next-5-days__date">
+				<div className="next-5-days">
+					<h2 className="next-5-days__heading">Siguientes 5 días</h2>
+					<div className="next-5-days__container">
+						<div className="next-5-days__row">
+							<div className="next-5-days__date">
 								Lun
-								<div class="next-5-days__label">30/7</div>
+								<div className="next-5-days__label">30/7</div>
 							</div>
 
-							<div class="next-5-days__Min">
+							<div className="next-5-days__Min">
 								10&deg;
-								<div class="next-5-days__label">Min</div>
+								<div className="next-5-days__label">Min</div>
 							</div>
 
-							<div class="next-5-days__Max">
+							<div className="next-5-days__Max">
 								21&deg;
-								<div class="next-5-days__label">Max</div>
+								<div className="next-5-days__label">Max</div>
 							</div>
 
-							<div class="next-5-days__icon">
+							<div className="next-5-days__icon">
 								<img src={sunny} alt="Sunny" />
 							</div>
 
-							<div class="next-5-days__Lluvia">
+							<div className="next-5-days__Lluvia">
 								0%
-								<div class="next-5-days__label">Lluvia</div>
+								<div className="next-5-days__label">Lluvia</div>
 							</div>
 
-							<div class="next-5-days__Viento">
+							<div className="next-5-days__Viento">
 								12mph
-								<div class="next-5-days__label">Viento</div>
+								<div className="next-5-days__label">Viento</div>
 							</div>
 						</div>
-						<div class="next-5-days__row">
-							<div class="next-5-days__date">
+						<div className="next-5-days__row">
+							<div className="next-5-days__date">
 								Mar
-								<div class="next-5-days__label">31/7</div>
+								<div className="next-5-days__label">31/7</div>
 							</div>
 
-							<div class="next-5-days__Min">
+							<div className="next-5-days__Min">
 								9&deg;
-								<div class="next-5-days__label">Min</div>
+								<div className="next-5-days__label">Min</div>
 							</div>
 
-							<div class="next-5-days__Max">
+							<div className="next-5-days__Max">
 								18&deg;
-								<div class="next-5-days__label">Max</div>
+								<div className="next-5-days__label">Max</div>
 							</div>
 
-							<div class="next-5-days__icon">
+							<div className="next-5-days__icon">
 								<img src={mostlySunny} alt="Mayormente Soleado" />
 							</div>
 
-							<div class="next-5-days__Lluvia">
+							<div className="next-5-days__Lluvia">
 								3%
-								<div class="next-5-days__label">Lluvia</div>
+								<div className="next-5-days__label">Lluvia</div>
 							</div>
 
-							<div class="next-5-days__Viento">
+							<div className="next-5-days__Viento">
 								7mph
-								<div class="next-5-days__label">Viento</div>
+								<div className="next-5-days__label">Viento</div>
 							</div>
 						</div>
-						<div class="next-5-days__row">
-							<div class="next-5-days__date">
+						<div className="next-5-days__row">
+							<div className="next-5-days__date">
 								Mie
-								<div class="next-5-days__label">1/8</div>
+								<div className="next-5-days__label">1/8</div>
 							</div>
 
-							<div class="next-5-days__Min">
+							<div className="next-5-days__Min">
 								7&deg;
-								<div class="next-5-days__label">Min</div>
+								<div className="next-5-days__label">Min</div>
 							</div>
 
-							<div class="next-5-days__Max">
+							<div className="next-5-days__Max">
 								15&deg;
-								<div class="next-5-days__label">Max</div>
+								<div className="next-5-days__label">Max</div>
 							</div>
 
-							<div class="next-5-days__icon">
+							<div className="next-5-days__icon">
 								<img src={mostlySunny} alt="Mayormente Soleado" />
 							</div>
 
-							<div class="next-5-days__Lluvia">
+							<div className="next-5-days__Lluvia">
 								75%
-								<div class="next-5-days__label">Lluvia</div>
+								<div className="next-5-days__label">Lluvia</div>
 							</div>
 
-							<div class="next-5-days__Viento">
+							<div className="next-5-days__Viento">
 								11mph
-								<div class="next-5-days__label">Viento</div>
+								<div className="next-5-days__label">Viento</div>
 							</div>
 						</div>
-						<div class="next-5-days__row">
-							<div class="next-5-days__date">
+						<div className="next-5-days__row">
+							<div className="next-5-days__date">
 								Jue
-								<div class="next-5-days__label">2/8</div>
+								<div className="next-5-days__label">2/8</div>
 							</div>
 
-							<div class="next-5-days__Min">
+							<div className="next-5-days__Min">
 								12&deg;
-								<div class="next-5-days__label">Min</div>
+								<div className="next-5-days__label">Min</div>
 							</div>
 
-							<div class="next-5-days__Max">
+							<div className="next-5-days__Max">
 								24&deg;
-								<div class="next-5-days__label">Max</div>
+								<div className="next-5-days__label">Max</div>
 							</div>
 
-							<div class="next-5-days__icon">
+							<div className="next-5-days__icon">
 								<img src={sunny} alt="Sunny" />
 							</div>
 
-							<div class="next-5-days__Lluvia">
+							<div className="next-5-days__Lluvia">
 								2%
-								<div class="next-5-days__label">Lluvia</div>
+								<div className="next-5-days__label">Lluvia</div>
 							</div>
 
-							<div class="next-5-days__Viento">
+							<div className="next-5-days__Viento">
 								8mph
-								<div class="next-5-days__label">Viento</div>
+								<div className="next-5-days__label">Viento</div>
 							</div>
 						</div>
-						<div class="next-5-days__row">
-							<div class="next-5-days__date">
+						<div className="next-5-days__row">
+							<div className="next-5-days__date">
 								Vie
-								<div class="next-5-days__label">30/7</div>
+								<div className="next-5-days__label">30/7</div>
 							</div>
 
-							<div class="next-5-days__Min">
+							<div className="next-5-days__Min">
 								10&deg;
-								<div class="next-5-days__label">Min</div>
+								<div className="next-5-days__label">Min</div>
 							</div>
 
-							<div class="next-5-days__Max">
+							<div className="next-5-days__Max">
 								21&deg;
-								<div class="next-5-days__label">Max</div>
+								<div className="next-5-days__label">Max</div>
 							</div>
 
-							<div class="next-5-days__icon">
+							<div className="next-5-days__icon">
 								<img src={mostlySunny} alt="Mayormente Soleado" />
 							</div>
 
-							<div class="next-5-days__Lluvia">
+							<div className="next-5-days__Lluvia">
 								0%
-								<div class="next-5-days__label">Lluvia</div>
+								<div className="next-5-days__label">Lluvia</div>
 							</div>
 
-							<div class="next-5-days__Viento">
+							<div className="next-5-days__Viento">
 								12mph
-								<div class="next-5-days__label">Viento</div>
+								<div className="next-5-days__label">Viento</div>
 							</div>
 						</div>
 					</div>
